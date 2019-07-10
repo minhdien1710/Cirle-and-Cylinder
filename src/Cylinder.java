8eb7208953944222cf4c1484e62414ed57bf1867
minhdien1710 <nguyenminhdien996@gmail.com>
2019-07-10 09:47:23 +0700

    added

---
+++ src/Cylinder.java
@@ -0,0 +1,28 @@
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(){
+    }
+    public Cylinder(double height){
+        this.height = height;
+    }
+    public Cylinder(double radius, double height,String color){
+        super(radius,color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getVolume(){
+        return super.getArea()*this.height;
+    }
+    @Override
+    public String toString(){
+        return "A " + getColor() + " Cylinder with radius = " + getRadius() + ", height = " + getHeight() +" has volume = "+ getVolume();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(30, "red");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(10,20,"green");
+        System.out.println(cylinder);
+    }
+}
